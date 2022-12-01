3cd9d99ea119f908eb23edaf8f6fa8f0424d3843
root <root@ip-172-31-90-8.ec2.internal>
2022-12-01 22:10:24 +0000

    added login.php

--- login.php
+++ login.php
@@ -1,6 +1,17 @@
 <html>
+<link rel='stylesheet' href='/var/www/html/Zuckerbots.css'>
+<header
+<h1>Krieder WorkShop</h1>
+</header>
+<nav> 
+ <ul>
+  <li><a href="add.php">Add</a></li>
+  <li><a href="delete.php">Delete</a></li>
+  <li><a href="show.php">Show</a></li>
+</ul>
+</nav>
 <form action="process-login.php">
 	Item ID: <input type="text" name="id"><br>
 	<input type="submit" value="submit">
 </form>
-<html>
+</html>
